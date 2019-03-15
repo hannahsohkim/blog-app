b88f695073cfd3f8af46b9e1f88bb9ee36bd1291
Hannah <hannahskim@berkeley.edu>
2019-03-15 07:50:40 +0000

    Add styling with semantic-ui

--- app.js
+++ app.js
@@ -3,17 +3,71 @@ var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
+// APP CONFIG
 mongoose.connect('mongodb://localhost/restful_blog_app', {useNewUrlParser: true});
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
-app.use(bodyParser.urlEncoded({extended: true}));
+app.use(bodyParser.urlencoded({extended: true}));
 
+
+
+// MONGOOSE/MODEL CONFIG
 var blogSchema = new mongoose.Schema({
     title: String, 
     image: String, 
     body: String,
-    created: Date()
+    created: {type: Date, default: Date.now}
+});
+
+var Blog = mongoose.model('Blog', blogSchema);
+
+// Blog.create({
+//     title: "My First Blog", 
+//     image: 'https://images.unsplash.com/photo-1551636898-47668aa61de2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+//     body: "I'm so excited to be writing and publishing my first blog post! Can't wait to start posting more and building this blog out. Thanks for reading!",
+// });
+
+
+
+
+//RESTFUL ROUTES
+app.get('/', (req, res) => {
+    res.redirect('/blogs');
 })
+
+// INDEX - List all blogs
+app.get('/blogs', (req, res) => {
+    Blog.find({}, (err, blogs) => {
+        if (err) {
+            console.log("ERROR", err)
+        } else {
+        res.render('index', {blogs: blogs});
+        }
+    })
+})
+
+// NEW ROUTE
+app.get('/blogs/new', (req, res) => {
+    res.render('new');
+})
+
+// CREATE ROUTE
+app.post('/blogs', (req, res) => {
+    Blog.create(req.body.blog, (err, newBlog) => {
+        if (err) {
+            res.render('new');
+        } else {
+             res.redirect('/blogs');
+        }
+    })
+})
+
+// SHOW ROUTE 
+
+
+
+
+
 
 app.listen(process.env.PORT, process.env.IP, () => {
     console.log('Server is running')
